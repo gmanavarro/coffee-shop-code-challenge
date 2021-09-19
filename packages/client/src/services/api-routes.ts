7430615d2358fbca19b794fb7e0aa529cba10c94
German Navarro <g.manavarro@gmail.com>
2021-09-19 05:50:08 -0300

    Add axios client service and api routes, add util constants and toast notifications

---
+++ packages/client/src/services/api-routes.ts
@@ -0,0 +1,10 @@
+// Item routes
+export const GET_ITEMS_ROUTE = '/items';
+export const GET_ITEM_BY_ID_ROUTE = '/item/:id';
+
+// Order routes
+export const CREATE_ORDER_ROUTE = '/order';
+export const ADD_ITEM_TO_ORDER_ROUTE = '/order/:id/';
+export const GET_ORDER_BY_ID_ROUTE = '/order/:id';
+export const CONFIRM_ORDER_ROUTE = '/order/:id/confirm';
+export const ORDER_COMPLETED_EVENT_ROUTE = '/order/:id/completed';
---
+++ packages/client/src/utils/constants.ts
@@ -0,0 +1,7 @@
+export enum MessageType {
+  SUCCESS,
+  ERROR,
+}
+
+export const ORDER_CREATED_SUCCESS_MESSAGE = 'Order created!';
+export const ITEM_ADDED_SUCCESS_MESSAGE = 'Item added to order!';
